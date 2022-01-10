6ddc1ac0f8b75fab9523b0c0a1b06a5ec2559b5a
adrianetodesign <adrianetodesign@gmail.com>
2022-01-10 22:52:40 +0000

    Completed tail function and added test cases.

--- tail.js
+++ tail.js
@@ -1,3 +1,5 @@
+const { assert } = require("console");
+
 // assertEqual function copied from assertEqual.js
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
@@ -11,12 +13,36 @@ const tail = function(array) {
   if (array.length <= 1) {
     return [];
   } else {
-    let finalArr = array.slice(1);
-    return finalArr;
+    return array.slice(1);
   }
 };
 
-// Test Case: Check the original array 
-const words = ["Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+const test1 = ["Drink", "Deep", "and", "Descend"];
+const result1 = tail(test1);
+assertEqual(test1.length, 4);
+assertEqual(result1[0], "Deep");
+assertEqual(result1[1], "and");
+assertEqual(result1[2], "Descend");
+console.log("---");
+
+const test2 = [8,6,7,5,3,0,9];
+const result2 = tail(test2);
+assertEqual(test2.length, 7);
+assertEqual(result2[0], 6);
+assertEqual(result2[1], 7);
+assertEqual(result2[2], 5);
+assertEqual(result2[3], 3);
+assertEqual(result2[4], 0);
+assertEqual(result2[5], 9);
+console.log("---");
+
+const test3 = [1];
+const result3 = tail(test3);
+assertEqual(test3.length, 1);
+assertEqual(result3.length, 0);
+console.log("---");
+
+const test4 = [];
+const result4 = tail(test4);
+assertEqual(test4.length, 0);
+assertEqual(result4.length, 0);
