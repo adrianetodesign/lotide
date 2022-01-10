a5e9646d0fff4978c2ed963e7ecf0f0bb9533098
adrianetodesign <adrianetodesign@gmail.com>
2022-01-10 22:08:08 +0000

    Created head.js, and copied assertEqual function in to head.js

---
+++ head.js
@@ -0,0 +1,15 @@
+// assertEqual function copied from assertEqual.js
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const head = function(array) {
+
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
