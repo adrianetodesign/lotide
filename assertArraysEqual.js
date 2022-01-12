c91b4edfb489442e127a8849a74cbd9c385f10d7
adrianetodesign <adrianetodesign@gmail.com>
2022-01-12 21:44:08 +0000

    Added brackets inside the string for assertions, to make arrays more readable.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,9 +13,9 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`✅ Assertion passed: ${array1} ===  ${array2}`);
+    console.log(`✅ Assertion passed: [${array1}] ===  [${array2}]`);
   } else {
-    console.log(`🛑 Assertion failed: ${array1} !==  ${array2}`);
+    console.log(`🛑 Assertion failed: [${array1}] !==  [${array2}]`);
   }
 };
 
