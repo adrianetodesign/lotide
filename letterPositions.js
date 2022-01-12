65bff718242cea0c22279168a6938a14a8bf27dd
adrianetodesign <adrianetodesign@gmail.com>
2022-01-12 23:30:10 +0000

    Created letterPositions.js and initial implementation of letterPositions function.

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+// Copied from eqArrays.js
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//Copied from assertArraysEqual.js
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion passed: [${array1}] ===  [${array2}]`);
+  } else {
+    console.log(`🛑 Assertion failed: [${array1}] !==  [${array2}]`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === ' ') {
+      continue;
+    } else if (results[sentence[i]]){
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
