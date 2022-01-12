6d1ebcd38fdd4b5adc9d0a23d9da7f6dca2130d1
adrianetodesign <adrianetodesign@gmail.com>
2022-01-12 00:59:45 +0000

    Created eqArrays.js, and implemented eqArrays function.

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+// Copied from assertEqual.js
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false); // => should PASS
