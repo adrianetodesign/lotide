e1ea1be9f8b3ec23aaed3e219046941328541842
adrianetodesign <adrianetodesign@gmail.com>
2022-01-12 01:45:38 +0000

    Added more test cases to eqArrays.js:

--- eqArrays.js
+++ eqArrays.js
@@ -12,13 +12,15 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
-}
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
 assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
