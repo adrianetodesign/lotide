11295db00bd9ba7cf7ec919875384f1c01ee4a3a
adrianetodesign <adrianetodesign@gmail.com>
2022-01-13 00:28:42 +0000

    Created initial implementation of eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -8,5 +8,22 @@ const assertEqual = function(actual, expected) {
 }
 
 const eqObjects = function(object1, object2) {
-
+  if (object.keys(object1).length !== object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if(!object2[key]) {
+      return false;
+    } else if (object1[key] !== object2[key]){
+      return false;
+    }
+  }
+  return true;
 };
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
