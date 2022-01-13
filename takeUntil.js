1cb31a9f6da07f6af36b198e6c2cdc48c378c5db
adrianetodesign <adrianetodesign@gmail.com>
2022-01-13 23:52:47 +0000

    Completed initial implementeation of takeuUntil function.

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,25 @@
+// Copied from eqArrays.js
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Copied from assertArraysEqual.js
+const assertArraysEqual = function(array1, array2) {
+  let assertion = `🛑 Assertion failed: [${array1}] !==  [${array2}]`;
+  if (eqArrays(array1, array2)) {
+    assertion = `✅ Assertion passed: [${array1}] ===  [${array2}]`;
+  }
+  console.log(assertion);
+};
+
 const takeUntil = function(array, callback) {
   results = [];
   for (i = 0; i < array.length; i++) {
@@ -9,3 +31,12 @@ const takeUntil = function(array, callback) {
   return results;
 };
 
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2); 
