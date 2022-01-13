dc6fb1f604d05e888b22455b720532f970ac31b9
adrianetodesign <adrianetodesign@gmail.com>
2022-01-13 23:58:22 +0000

    added more test cases using assertions

--- takeUntil.js
+++ takeUntil.js
@@ -33,10 +33,12 @@ const takeUntil = function(array, callback) {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2); 
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+
+const data3 = ["Drink", "Deep", "and", "Descend"];
+const results3 = takeUntil(data3, x => x === "and");
+assertArraysEqual(results3, ["Drink", "Deep"]);
