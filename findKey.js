928bc2f2120ebc09de24cd76f0e4316ab49708fc
adrianetodesign <adrianetodesign@gmail.com>
2022-01-14 02:48:06 +0000

    Created findKey.js and implemented findKey function

---
+++ findKey.js
@@ -0,0 +1,35 @@
+// Copied from assertEqual.js
+const assertEqual = function(actual, expected) {
+  let assertion = `🛑 Assertion failed: ${actual} !==  ${expected}`;
+  if (actual === expected) {
+    assertion = `✅ Assertion passed: ${actual} ===  ${expected}`;
+  }
+  console.log(assertion);
+};
+
+
+const findKey = function (obj, callback) {
+  for (key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
