05f03f43089fcdcbb7c5dd147b018af33621c122
adrianetodesign <adrianetodesign@gmail.com>
2022-01-14 04:58:50 +0000

    Added test case to check for undefined.

--- findKey.js
+++ findKey.js
@@ -33,3 +33,12 @@ assertEqual(findKey({
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 3), "Akaleri");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 7), undefined);
