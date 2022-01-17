6588c187b2fbf7971109cea12e590901a6406a4c
adrianetodesign <adrianetodesign@gmail.com>
2022-01-17 23:29:52 +0000

    Refacted eqArrays.js and moved all test cases to test directory.

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// Copied from assertEqual.js
-const assertEqual = function(actual, expected) {
-  let assertion = `🛑 Assertion failed: ${actual} !==  ${expected}`;
-  if (actual === expected) {
-    assertion = `✅ Assertion passed: ${actual} ===  ${expected}`;
-  }
-  console.log(assertion);
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -19,8 +10,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-assertEqual(eqArrays([1, 2], [1, 2, 3]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
+module.exports = eqArrays;
