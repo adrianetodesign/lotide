b6faa1785011d4b1467001e0c86ca032fd896b67
adrianetodesign <adrianetodesign@gmail.com>
2022-01-18 01:35:00 +0000

    Removed unnecessary comments

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,6 @@
 const assert = require('chai').assert;
 const middle = require("../middle");
 
-// assertArraysEqual(middle([1]), []);
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-// assertArraysEqual(middle([1, 2 ,3 ,4 ,5 ,6]), [3, 4]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-
 describe("#middle", () => {
 
   it("Returns [] for [1]", () => {
